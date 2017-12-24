0e159694960d0f02d314993bc51d22c90eb7d449
Carlos Precioso <kalprestito@gmail.com>
2017-12-24 04:07:59 +0100

    Dinamically update age

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import "./index.pug"
+
+import * as age from "./age"
+
+var currentAge = "" + age
+for (var i = 0, list = document.getElementsByClassName("age"); i < list.length; i += 1) {
+  list[i].innerHTML = currentAge
+}
