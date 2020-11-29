7a259113a972db7093c7a8795a8b45a36b8c9a2c
Carlos Precioso <cprecioso@noreply.github.com>
2020-11-29 12:49:18 +0100

    Improve preview mode UX

--- src/pages/_preview.tsx
+++ src/pages/_preview.tsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled"
 import { GetStaticProps, NextPage } from "next"
+import Head from "next/head"
 import Link from "next/link"
+import { theme } from "../helpers/theme"
 
 const Input = styled.input`
   color: inherit;
@@ -15,12 +17,19 @@ const Input = styled.input`
   }
 `
 
+const Hr = styled.hr`
+  border: 1px solid ${theme.currentColor.foreground};
+`
+
 const Button = Input.withComponent("button")
 
 type Props = { isPreview: boolean }
 
 const Preview: NextPage<Props> = ({ isPreview }) => (
   <div>
+    <Head>
+      <title>Preview mode</title>
+    </Head>
     <h1>Preview mode</h1>
     <p>Preview mode is {isPreview ? "enabled" : "disabled"}</p>
     {isPreview ? (
@@ -38,6 +47,20 @@ const Preview: NextPage<Props> = ({ isPreview }) => (
         <Input type="submit" />
       </form>
     )}
+    <Hr />
+    <p>Go to:</p>
+    <ul>
+      <Link href="/">
+        <a>
+          <li>Home</li>
+        </a>
+      </Link>
+      <Link href="/blog">
+        <a>
+          <li>Blog</li>
+        </a>
+      </Link>
+    </ul>
   </div>
 )
 
--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -2,9 +2,9 @@ import { NextApiHandler } from "next"
 
 export default <NextApiHandler>((req, res) => {
   if (req.body.password === process.env.PREVIEW_MODE_PASSWORD) {
-    res.setPreviewData({}).writeHead(307, { Location: "/" }).end()
+    res.setPreviewData({}).writeHead(307, { Location: "/_preview" }).end()
   } else if (!req.body.password) {
-    res.clearPreviewData().writeHead(307, { Location: "/" }).end()
+    res.clearPreviewData().writeHead(307, { Location: "/_preview" }).end()
   } else {
     res.clearPreviewData().status(401).json({ message: "Invalid token" })
   }
