e9fd6571c70226f2f673048317b84816e26ebc0b
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2021-12-28 14:17:50 +0100

    Simplify client

--- src/api/_gql.ts
+++ src/api/_gql.ts
@@ -1,20 +1,27 @@
-import { GraphQLClient } from "graphql-request"
-
 export const gql = String.raw
 
-const makeClient = (endpoint: string) =>
-  new GraphQLClient(endpoint, {
-    headers: { Authorization: `Bearer ${process.env.DATO_API_TOKEN}` },
-  })
+const publicEndpoint = "https://graphql.datocms.com/"
+const previewEndpoint = "https://graphql.datocms.com/preview"
 
-const publicClient = makeClient("https://graphql.datocms.com/")
-const previewClient = makeClient("https://graphql.datocms.com/preview")
-
-export const request = (
+export const request = async (
   query: string,
   variables?: Record<string, any>,
   previewMode?: boolean
-): Promise<any> => {
-  const client = previewMode ? previewClient : publicClient
-  return client.request(query, variables)
+) => {
+  const endpoint = previewMode ? previewEndpoint : publicEndpoint
+
+  const res = await fetch(endpoint, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.DATO_API_TOKEN}`,
+    },
+    body: JSON.stringify({ query, variables }),
+  })
+
+  const { data, errors }: { data?: any; errors?: string[] } = await res.json()
+
+  if (errors) throw errors
+
+  return data
 }
