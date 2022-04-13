1319d35d40f255f5d4f6a8baa23708890aead5eb
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2022-04-13 22:55:39 +0200

    Use simpler color functions

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,16 +1,16 @@
+import { hex2hsl } from "@csstools/convert-colors"
 import { FunctionComponent, useMemo } from "react"
 import { ButtonModel } from "../../api/homepage"
-import { hexToRgb, rgbToHsl } from "../../helpers/colors"
 import styles from "./styles.module.css"
 
-const lightness = (hex: string) => rgbToHsl(hexToRgb(hex)).l / 100
+const lightness = (hex: string) => hex2hsl(hex)[2] / 100
 
 export const Button: FunctionComponent<{ button: ButtonModel }> = ({
   button,
 }) => {
   const cssVariables: Record<string, string> = useMemo(() => {
     const shadowColor =
-      lightness(button.accentColor.hex) === 1
+      lightness(button.accentColor.hex) >= 0.8
         ? button.backgroundColor.hex
         : button.accentColor.hex
 
---
+++ types/csstools__convert-colors.d.ts
@@ -0,0 +1,3 @@
+declare module "@csstools/convert-colors" {
+  export const hex2hsl: (hex: string) => [h: number, s: number, l: number]
+}
