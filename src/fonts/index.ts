ccfaa132f622d82d46070dea2ff87ecb2a3cc744
Carlos Precioso <511681+cprecioso@users.noreply.github.com>
2022-12-03 00:30:26 +0100

    Use font-display: swap

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -3,6 +3,7 @@ import localFont from "@next/font/local"
 
 export const zillaSlab = Zilla_Slab({
   variable: "--font-zilla-slab",
+  display: "swap",
   weight: ["300"],
   style: ["normal", "italic"],
   subsets: ["latin", "latin-ext"],
@@ -10,6 +11,7 @@ export const zillaSlab = Zilla_Slab({
 
 export const fuji = localFont({
   variable: "--font-fuji",
+  display: "swap",
   weight: "800",
   style: "normal",
   src: "./assets/Fuji-Bold.woff2",
